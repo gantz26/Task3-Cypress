5395eec0b92caaaad5ed30f2fdb6dffdf67c9211
Bohdan Honchar <bohdan26062003@gmail.com>
2024-07-19 15:55:15 +0300

    Cypress results are recorded to Cloud

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: 'gfv5am',
   reporter: 'cypress-mochawesome-reporter',
   e2e: {
     setupNodeEvents(on, config) {
